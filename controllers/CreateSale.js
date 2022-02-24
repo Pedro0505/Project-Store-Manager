1978ed70e157c1c43040c2f2f9bd58b5c90b6723
Pedro Henrique <pedrohrds05@hotmail.com>
2022-02-24 14:32:03 -0300

    fix: tentativa de consertar os requisitos

--- controllers/CreateSale.js
+++ controllers/CreateSale.js
@@ -3,7 +3,7 @@ const SalesModel = require('../models/SalesModel');
 const CreateSale = async (req, res) => {
   const { id } = await SalesModel.createSale();
 
-  req.body.forEach(async ({ productId, quantity }) => {
+  req.body.map(async ({ productId, quantity }) => {
     await SalesModel.createSaleProduct({ productId, quantity, id });
   });
 
