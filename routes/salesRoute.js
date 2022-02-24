08931a4120c0fd11a6313b945fb75f513a300762
Pedro Henrique <pedrohrds05@hotmail.com>
2022-02-24 14:46:50 -0300

    fix: tentativa de consertar os requisitos

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -6,7 +6,7 @@ const salesRoute = express.Router();
 
 salesRoute.get('/', SalesControllers.getAllSales);
 salesRoute.get('/:id', SalesControllers.getByIdSales);
-salesRoute.post('/', SalesMiddlewares.SalesMiddleware, SalesControllers.CreateSale);
+salesRoute.post('/', SalesMiddlewares.SalesMiddleware);
 salesRoute.put('/:id', SalesMiddlewares.SalesMiddleware);
 
 module.exports = salesRoute;
